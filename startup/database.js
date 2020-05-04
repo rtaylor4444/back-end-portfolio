a5c9d69dc6cfced0c680700f52bc349d879a8110
Rob <rtaylor4444@gmail.com>
2020-05-04 04:18:16 -0400

    Got rid of small console.log

--- startup/database.js
+++ startup/database.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 module.exports = function () {
-  console.log(config.get("DB_Connection"));
   mongoose
     .connect(config.get("DB_Connection"), {
       useNewUrlParser: true,
