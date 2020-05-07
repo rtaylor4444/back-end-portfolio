2dc66a1ad5a0725f3f889a4fe08ed233d39fb26b
Rob Taylor <rtaylor4444@gmail.com>
2020-05-07 08:10:21 -0400

    jwt now includes the users email

--- mongoose_models/user.js
+++ mongoose_models/user.js
@@ -38,7 +38,12 @@ const userSchema = new mongoose.Schema({
 });
 userSchema.methods.generateAuthToken = function () {
   const token = jwt.sign(
-    { _id: this._id, name: this.name, isAdmin: this.isAdmin },
+    {
+      _id: this._id,
+      name: this.name,
+      email: this.email,
+      isAdmin: this.isAdmin,
+    },
     config.get("jwtPrivateKey")
   );
   return token;
