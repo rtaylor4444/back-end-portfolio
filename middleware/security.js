cf1728090cf253dcf5a06a8b6ee521b9721011bc
Rob Taylor <rtaylor4444@gmail.com>
2020-05-21 22:41:14 -0400

    Updated cors to allow put method

--- middleware/security.js
+++ middleware/security.js
@@ -3,6 +3,7 @@
 module.exports = function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Expose-Headers", "x-auth-token");
   next();
 };
